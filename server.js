10df4067422ac6a56b4fbfc4f544c484588ae8e5
karthick <karthickkumar7.k7@gmail.com>
2021-12-31 07:17:55 -0800

    second commit

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 6969;
 
 app.get("/", (req, res) => {
 	res.send(
-		'<h1 style="padding:20px;" > <span style="color:red;">Chethan</span> is a bitch boy mangina cuck <span style="color:orange;">simp</span></h1>'
+		'<h1 style="padding:20px;" > <span style="color:red;">Chethan</span> is a bitch boy man<span style="color:pink;">gina</span> cuck <span style="color:orange;">simp</span></h1>'
 	);
 });
+
+//https://cryptic-everglades-03821.herokuapp.com/
+// https://git.heroku.com/cryptic-everglades-03821.git
 
 app.listen(PORT, () => console.log("server started..."));
