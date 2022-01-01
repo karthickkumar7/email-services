16a693405ef497697badfcd74c2182131105b2b9
karthick <karthickkumar7.k7@gmail.com>
2022-01-01 06:11:15 -0800

    mongo initialized

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 const express = require("express");
+const passport = require("passport");
+const googleStategy = require("passport-google-oauth20").Strategy;
+const { connect } = require("mongoose");
+
+const {
+	googleClientID,
+	googleClientSecret,
+	mongoUrlString,
+} = require("./config/keys");
+
+// https://cryptic-everglades-03821.herokuapp.com/
+// https://git.heroku.com/cryptic-everglades-03821.git
 
 const app = express();
 
 const PORT = process.env.PORT || 6969;
 
-app.get("/", (req, res) => {
-	res.send(
-		'<h1 style="padding:20px;" > <span style="color:red;">Chethan</span> is a bitch boy man<span style="color:pink;">gina</span> cuck <span style="color:orange;">simp</span></h1>'
-	);
-});
+passport.use(
+	new googleStategy(
+		{
+			clientID: googleClientID,
+			clientSecret: googleClientSecret,
+			callbackURL: "/auth/google/callback",
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log("accessToken", accessToken),
+				console.log("refreshToken", refreshToken),
+				console.log("profile", profile);
+		}
+	)
+);
 
-//https://cryptic-everglades-03821.herokuapp.com/
-// https://git.heroku.com/cryptic-everglades-03821.git
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["profile", "email"],
+	})
+);
 
-app.listen(PORT, () => console.log("server started..."));
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+connect(
+	mongoUrlString,
+	{ useNewUrlParser: true, setUnifiedTopology: true },
+	() => app.listen(PORT, () => console.log("dbconnected/server started..."))
+);
